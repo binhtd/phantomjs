e220f02c7e5e3092e178aec4e28fd464f5ec1581
binhtd <trinhducbinh@gmail.com>
2016-08-23 17:17:30 +0700

    continue using phantomjs

---
+++ codeevaluation1.js
@@ -0,0 +1,13 @@
+var page = require("webpage").create();
+
+page.onConsoleMessage = function(msg) {
+	console.log("Page title is " + msg);
+};
+
+page.open('http://dantri.com.vn', function(status) {
+	page.evaluate(function() {
+		console.log(document.title);
+	});
+
+	phantom.exit();
+});
---
+++ screencapture.js
@@ -0,0 +1,8 @@
+var page = require("webpage").create();
+page.viewportSize = {width:1024, height: 768};
+
+page.clipRect = {top: 0, left: 0, width: 1024, height:768};
+page.open('https://github.com', function() {
+	page.render("./github.png");
+	phantom.exit();
+});
